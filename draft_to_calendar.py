d7b7cea3a9e4c3bfe8c6642fe6bfad44a7f5cf6a
juana5678 <118631590+juana5678@users.noreply.github.com>
2022-12-26 16:52:38 -0600

    Update draft_to_calendar.py

--- draft_to_calendar.py
+++ draft_to_calendar.py
@@ -5,7 +5,7 @@ from bs4 import BeautifulSoup
 import re
 import socket
 
-from aiohttp_socks import ProxyConnector
+from aiohttp_ import ProxyConnector
 from python_socks import ProxyType
 from datetime import date
 
